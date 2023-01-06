7057f2edec1db0d2cc4e18ae5f4f2e2f332e828f
SumitRaj01 <Sumeet23051994@gmail.com>
2023-01-06 13:10:19 +0530

    class 2-igniting our app
    Learned about parcel, dependencies and transitive dependencies and many more

--- Class-1/App.js
+++ Class-1/App.js
@@ -1,3 +1,31 @@
+/* ** PARCEL IS DOING ALL THE THINGS MENTIONED BELOW
+*Creates a server 
+*HMR: Hot Module Replacement: parcel keeps a track of all the changes made in a file and it tells the server to reload
+*File Watcher Algorithm(FWA): Using FWA parcel does HMR. FWA is written in C++
+*dist folder: Keeps the file minified for us. When we write npx parcel index.html it just creates faster devdelopment version of our project and serves it on the server. When we write npx parcel build index.html , a production build, it creates a lot of things it will minify your file and it will push this build to dist folder
+*BUNDLING
+*MINIFY
+*Cleaning our code
+*Dev and Production Build
+*Super fast build algorithm
+*Image Optimization
+*Caching while development
+*Compression
+*Compatible with older version of browser
+*TO enable HTTPS on dev like npx parcel index.html --https
+*manages port number
+*Consistent hashing Algorithm
+*Zero Config: you dont need a lot of configuration
+*
+*
+*
+*Tranisitive dependency: We have our package manager which handles and takes care of our transitive dependency of our code
+*/
+
+
+
+import React from "react";
+import ReactDOM  from "react-dom/client";
 const heading = React.createElement(
     "h1",
     {

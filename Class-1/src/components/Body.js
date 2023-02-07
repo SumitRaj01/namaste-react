a642f367bda1f892159f343a7281bd0a5cade517
SumitRaj01 <Sumeet23051994@gmail.com>
2023-02-07 12:42:47 +0530

    Class-6(Exploring the world)
    learned about useEffect hook , Shimmer Ui

--- Class-1/src/components/Body.js
+++ Class-1/src/components/Body.js
@@ -1,27 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { restaurantList } from "../contants";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 //What is state?
 //What is React Hooks? -Functions
 //What is useState?
 
 function filterData(searchText,restaurants){
-    const filterData= restaurants.filter((restaurant)=>restaurant.data.name.includes(searchText));
+    const filterData= restaurants.filter((restaurant)=>restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase()));
     return filterData;
 }
  
 const Body = () => {
     // const searchTxt="KFC";
-
+    
     //SearchText is a local state variable
-    const [restaurants, setRestaurants] = useState(restaurantList);
-
+    const  [allRestaurants,setAllRestaurants]=useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
     const [searchText, setSearchText] = useState("");	//To create state variable, returns=[variable name, function to update the variable]
 
 
+    //empty dependency array=>once after render
+    // dep array [searchText]=>once after initial render+everytime after rerender when my search text changes
+    useEffect(()=>{
+        //API call
+        getRestaurants();
+    },[]);
 
-    return (
+    async function getRestaurants(){
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING");
+        const json=await data.json();
+        console.log(json);
+        setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+        setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+    }
+
+    console.log("render");
+
+    //Conditional Rendering
+    //if restaurant is empty => shimmer UI
+    //if restaurant has data => actual data UI
+    
+    //not render component (Early return)
+    if(!allRestaurants) return null;
+    
+    if(filteredRestaurants?.length===0) return <h1>No Restaurant match your Filter!!</h1>
+
+    return (allRestaurants?.length===0)?<Shimmer/>: (
         <>
             <div className="search-container">
                 <input type="text" className="search-input" placeholder="Search" value={searchText}
@@ -33,15 +59,16 @@ const Body = () => {
                 <button className="search-btn" onClick={() => {
 
                     //need to filter the data
-                    const data=filterData(searchText,restaurants);
+                    const data=filterData(searchText,allRestaurants);
                     //update the state-restaurants
-                    setRestaurants(data);
+                    setFilteredRestaurants(data);
                 }
                 }>Search</button>
             </div>
             <div className="restaurant-list">
+            {/* You have to wtite logic for NO restaurant found here */}
                 {
-                    restaurants.map(restaurant => {
+                    filteredRestaurants.map(restaurant => {
                         return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
                     })
                 }
--- Class-1/src/components/Header.js
+++ Class-1/src/components/Header.js
@@ -1,4 +1,10 @@
- const Title = () => (
+import { useState } from "react";
+const loggedInUser=()=>{
+  //API call to check authentication
+  return false;
+}
+
+const Title = () => (
 
     <a href="/">
       <img className="logo" src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj" alt="logo" />
@@ -6,10 +12,10 @@
   );
 
   const Header = () => {
+    const [isLoggedIn,setIsLoggedIn]=useState(false);
     return (
       <div className="header">
         <Title />
-        
         <div className="nav-items">
           <ul>
             <li>Home</li>
@@ -18,6 +24,11 @@
             <li>Cart</li>
           </ul>
         </div>
+        {
+          isLoggedIn?(<button onClick={()=>setIsLoggedIn
+          (false)}>Logout</button>):(<button onClick={()=>setIsLoggedIn
+          (true)}>Login</button>)
+        }
       </div>
     )
   };
